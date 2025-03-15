898ffcf0857ca0b44cc9610355dff6e9b334e98f
BSIT-DURAN <bobduran5912024@gmail.com>
2025-03-15 09:49:07 +0800

    repbtn

--- instructor.php
+++ instructor.php
@@ -99,7 +99,7 @@ $current_image .= "?t=" . time();
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Racing+Sans+One&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
-
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
     <style>
     body {
     margin: 0;
