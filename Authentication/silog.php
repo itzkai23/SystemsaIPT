d1bd440a913cd8d50be0487151fa9db81ac70e80
BSIT-DURAN <bobduran5912024@gmail.com>
2025-04-25 15:55:36 +0800

    schedmodalcss

--- Authentication/silog.php
+++ Authentication/silog.php
@@ -20,6 +20,7 @@ if (isset($_SESSION['user_name'])) {
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
     <link rel="stylesheet" href="../css/cbot.css">
     <link rel="stylesheet" href="../css/silog.css">
+    <link rel="stylesheet" href="../css/sched.css">
 
 </head>
 <body>
